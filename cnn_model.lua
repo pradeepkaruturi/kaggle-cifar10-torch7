dad86ac58fbed6e27013a47b870e5df49f3d320d
pradeep <nandu@NandinisMacBookPro.local>
2014-12-07 14:01:40 -0500

    cnn model change

--- cnn_model.lua
+++ cnn_model.lua
@@ -13,11 +13,15 @@ function cnn_model() -- validate.lua Acc: 0.88
    model:add(nn.ReLU())
    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
    
+   model:add(nn.SpatialZeroPadding(1, 1, 1, 1))
+   model:add(nn.SpatialConvolutionMM(128, 256, 4, 4, 1, 1))
+   model:add(nn.ReLU())
+   
    -- fully connected layers
-   model:add(nn.SpatialConvolutionMM(128, 256, 2, 2, 1, 1))
+   model:add(nn.SpatialConvolutionMM(256, 512, 2, 2, 1, 1))
    model:add(nn.ReLU())
    model:add(nn.Dropout(0.5))
-   model:add(nn.SpatialConvolutionMM(256, 10, 1, 1, 1, 1))
+   model:add(nn.SpatialConvolutionMM(512, 10, 1, 1, 1, 1))
    
    model:add(nn.Reshape(10))
    model:add(nn.SoftMax())
