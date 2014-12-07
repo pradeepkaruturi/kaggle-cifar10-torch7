0943c471af1424a348fca898f113136cca412f55
pradeep <nandu@NandinisMacBookPro.local>
2014-12-07 16:15:21 -0500

    cnn lower number of features

--- cnn_model.lua
+++ cnn_model.lua
@@ -14,14 +14,14 @@ function cnn_model() -- validate.lua Acc: 0.88
    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
    
    model:add(nn.SpatialZeroPadding(1, 1, 1, 1))
-   model:add(nn.SpatialConvolutionMM(16, 32, 4, 4, 1, 1))
+   model:add(nn.SpatialConvolutionMM(16, 16, 4, 4, 1, 1))
    model:add(nn.ReLU())
    
    -- fully connected layers
-   model:add(nn.SpatialConvolutionMM(32, 64, 2, 2, 1, 1))
+   model:add(nn.SpatialConvolutionMM(16, 32, 2, 2, 1, 1))
    model:add(nn.ReLU())
    model:add(nn.Dropout(0.5))
-   model:add(nn.SpatialConvolutionMM(64, 10, 1, 1, 1, 1))
+   model:add(nn.SpatialConvolutionMM(32, 10, 1, 1, 1, 1))
    
    model:add(nn.Reshape(10))
    model:add(nn.SoftMax())
