279ca1c37c8fa5dd453e0f1a1f2620b770019bd1
pradeep <nandu@NandinisMacBookPro.local>
2014-12-07 14:11:54 -0500

    cnn modified 128 final channels

--- cnn_model.lua
+++ cnn_model.lua
@@ -5,23 +5,23 @@ function cnn_model() -- validate.lua Acc: 0.88
    local model = nn.Sequential() 
    
    -- convolution layers
-   model:add(nn.SpatialConvolutionMM(3, 32, 5, 5, 1, 1))
+   model:add(nn.SpatialConvolutionMM(3, 16, 5, 5, 1, 1))
    model:add(nn.ReLU())
    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
    
-   model:add(nn.SpatialConvolutionMM(32, 64, 5, 5, 1, 1))
+   model:add(nn.SpatialConvolutionMM(16, 32, 5, 5, 1, 1))
    model:add(nn.ReLU())
    model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
    
    model:add(nn.SpatialZeroPadding(1, 1, 1, 1))
-   model:add(nn.SpatialConvolutionMM(64, 128, 4, 4, 1, 1))
+   model:add(nn.SpatialConvolutionMM(32, 64, 4, 4, 1, 1))
    model:add(nn.ReLU())
    
    -- fully connected layers
-   model:add(nn.SpatialConvolutionMM(128, 256, 2, 2, 1, 1))
+   model:add(nn.SpatialConvolutionMM(64, 128, 2, 2, 1, 1))
    model:add(nn.ReLU())
    model:add(nn.Dropout(0.5))
-   model:add(nn.SpatialConvolutionMM(256, 10, 1, 1, 1, 1))
+   model:add(nn.SpatialConvolutionMM(128, 10, 1, 1, 1, 1))
    
    model:add(nn.Reshape(10))
    model:add(nn.SoftMax())
